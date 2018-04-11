12c6960ebb6256370ec9408e4b025087b78d42b9
shaggyzard <sebastian.ganto@pucp.pe>
2018-04-11 18:47:08 -0500

    primero :v:

---
+++ java/Panaderia.java
@@ -0,0 +1,58 @@
+importa java.util.ArrayList
+
+public class Panaderia{
+	private int idPanaderia;
+	private String nombre;
+	private String direccion;
+	private int telefono;
+	ArrayList<CuentaUsario> usuarios;
+	ArrayList<Almacen> almacenes;
+	ArrayList<Insumo> insumos;
+	ArrayList<cliente> clientes;
+	
+	public Panaderia(int idPanaderia,String nombre,String direccion,int telefono){
+		this.idPanaderia=idPanaderia;
+		this.nombre=nombre;
+		this.direccion=direccion;
+		this.telefono=telefono;
+		clientes = new ArrayList<Cliente>;
+		usarios = new ArrayList<CuentaUsario>;
+		almacenes = new ArrayList<Almacen>;
+		insumos = new ArrayList<Insumo>;
+	}
+	
+	public int getidPanaderia(){
+		return idPanaderia; 
+	}
+	
+	public void setidPanaderia(int idPanaderia){
+		this.idPanaderia=idPanaderia;
+	}
+	
+	public String getNombre(){
+		return nombre;
+	}
+	
+	public void setNombre(String nombre){
+		this.nombre=nombre;
+	}
+	
+	public String getDireccion(){
+		return direccion;
+	}
+	
+	public void setDireccion(String direccion){
+		this.direccion=direccion;
+	}
+	
+	public int getTelefono(){
+		return telefono;
+	}
+	
+	public int setTelefono(int telefono){
+		this.telefono=telefono;
+	}
+	
+	
+	
+}
---
+++ java/Permiso.java
@@ -0,0 +1,30 @@
+public class Permiso{
+	private int idPermiso;
+	private String nombre;
+	private String descripcion;
+
+	public int getIdPermiso() {
+        return idPermiso;
+    }
+	
+    public void setIdPermiso(int idPermiso) {
+        this.idPermiso = idPermiso;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+  
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
